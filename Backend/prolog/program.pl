6407b1e6c3cd2fb6681ad3b6e9bc1e8e3edce4c0
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-03-21 18:08:00 -0600

    delete trash prolog file that Misael forgot

--- Backend/prolog/program.pl
+++
@@ -1,9 +0,0 @@
-padre(juan,pedro).
-padre(pedro,luis).
-padre(carlos,juan).
-
-madre(ana,pedro).
-madre(maria,juan).
-
-abuelo(X,Y) :- padre(X,Z), padre(Z,Y).
-abuela(X,Y) :- madre(X,Z), padre(Z,Y).
